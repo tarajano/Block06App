bb10aaacf3e10ad1d421f811569b2a884e6e9d3c
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-25 21:10:26 -0400

    SUMMARY: Initial commit. Ads to layout: SeekBar, two ImageViews. Ads to MainActivity: OnSeekBarChangeListener.

---
+++ app/src/main/java/com/example/tarajano/block06app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tarajano.block06app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView02 = (ImageView) findViewById(R.id.imageView02);
+        imageView02.setImageResource(R.drawable.smile);
+
+        SeekBar seekBarColor = (SeekBar) findViewById(R.id.seekBar);
+        seekBarColor.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
